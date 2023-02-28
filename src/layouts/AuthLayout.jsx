241ed21839a25fa2a067547df622997737e36a41
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-28 12:56:29 +0100

    AuthLayout.jsx => change <Redirect/> to <Navigate/>

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // Librares
 import { useSelector } from "react-redux";
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
 // Components
 import Card from "../components/Card";
 // Store
@@ -11,9 +11,9 @@ const AuthLayout = () => {
     // let { path } = useRouteMatch();
     const isLoggedIn = useSelector(isLoggedInSelector());
 
-    // if (isLoggedIn) {
-        // return <Redirect to='/' />;
-    // }
+    if (isLoggedIn) {
+        return <Navigate to='/' />;
+    }
 
     return (
         <div className='flex grow flex-col justify-center items-center  dark:text-slate-200 '>
